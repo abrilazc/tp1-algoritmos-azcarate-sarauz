d96e11f12de53a88d1631f3bf7566c632af46cae
abril <abrilazcarate0@gmail.com>
2022-07-19 22:12:04 -0300

    v1

---
+++ game.h
@@ -0,0 +1,8 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include "polilinea.h"
+
+void render_nave(polilinea_t *nave, polilinea_t *chorro, float angulo, float *pos);
+
+#endif
--- polilinea.h
+++ polilinea.h
@@ -1,5 +1,5 @@
-#ifndef POLILINEA_H
-#define POLILINEA_H
+#ifndef _POLILINEA_H_
+#define _POLILINEA_H_
 
 #include <stdint.h>
 #include <stddef.h>
@@ -13,6 +13,9 @@ polilinea_t *polilinea_crear_vacia(size_t n);
 void polilinea_destruir(polilinea_t *polilinea);
 
 bool polilinea_setear_punto(polilinea_t *polilinea, size_t pos, float x, float y);
+
 bool polilinea_setear_color(polilinea_t *polilinea, color_t color);
+
+
 
 #endif

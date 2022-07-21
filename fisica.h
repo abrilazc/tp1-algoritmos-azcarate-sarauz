a008e0ee59bd491d1ebd02be843e884ebed9c2ca
abril <abrilazcarate0@gmail.com>
2022-07-21 18:55:36 -0300

    intell

--- fisica.h
+++ fisica.h
@@ -1,6 +1,10 @@
 #ifndef _FISICA_H_
 #define _FISICA_H_
 
+#include <stddef.h>
+#include <stdbool.h>
+
+
 void colision_rebote(float pos[2], float v[2], size_t dim[2]); //llamada en caso de colisiones que causan un rebote
 
 void acelerar(float v[2], float angulo, float a, float dt); //llamada para cambiar la velocidad de la nave
@@ -9,7 +13,7 @@ void gravedad(float v[2], bool planeta_infinito, float pos[2], float pos_g[2], f
 
 void mover(float pos[2],float v[2],float dt); //llamada para mover un objeto
 
-void rotar_nave(float pos[2], float angulo, bool horario, float rotacion_paso); //llamada para cambiar el angulo de la nave
+void rotar_nave(float pos[2], float *angulo, bool horario, float rotacion_paso); //llamada para cambiar el angulo de la nave
 
 bool colision(const float polilinea[][2], size_t n, float pos[2], float radio); //llamada para verificar si la nave chocó, return true si chocó
 
--- game.c
+++ game.c
@@ -15,9 +15,10 @@ typedef enum{
     TORRETA,
     REACTOR,
 }tipo_elementos_t;
+
 struct elemento {
     char nombre[20];
-
+    tipo_elementos_t tipo;
     bool infinito;
     size_t n_polilineas;
     polilinea_t *polilineas;

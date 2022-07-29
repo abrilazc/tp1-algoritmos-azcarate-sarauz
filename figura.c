35d82f402f77169b9504ad4319655000958c83f7
abril <abrilazcarate0@gmail.com>
2022-07-29 00:54:34 -0300

    1.0.10

--- figura.c
+++ figura.c
@@ -1,20 +1,14 @@
 #include "figura.h"
 
-#include <stdint.h>
-#include <stdio.h>
 
 //tipos de dato
 struct figura{
     char nombre[LONG_NOM];
-    figura_tipo_t figura;
+    figura_tipo_t tipo_figura;
     bool infinito;
     size_t cantidad_polilineas;
     polilinea_t **polilineas;
-    
 };
-
-typedef struct figura figura_t;
-
 const char* figura_tipo_a_cadena(figura_tipo_t figura){
     static const char* tipo[]={
     [ICONO]="Icono",
@@ -28,7 +22,7 @@ const char* figura_tipo_a_cadena(figura_tipo_t figura){
     return tipo[figura];
 }
 
-//creacion y destruccion
+//setters
 bool leer_encabezado_figura(FILE *f, char nombre[], figura_tipo_t *tipo, bool *infinito, size_t *cantidad_polilineas){
 
     if (!f) return false;
@@ -73,39 +67,6 @@ polilinea_t *leer_polilinea(FILE *f){
 
     return polilinea;
 }
-figura_t *figura_crear(FILE *f){
-    figura_t *figura=malloc(sizeof(figura_t));
-    if(figura==NULL) return NULL;
-    
-    if(leer_encabezado_figura(f,figura->nombre,&figura->figura,&figura->infinito,&figura->cantidad_polilineas)==0) return NULL;
-
-    size_t cantidad_polilineas=figura->cantidad_polilineas;
-
-    polilinea_t **conjunto_polilineas=malloc(sizeof(polilinea_t*)*cantidad_polilineas);
-    size_t i=0;
-    //polilinea_t *polilinea=leer_polilinea(f);
-    //figura->polilinea[i]=polilinea;
-    while(i<cantidad_polilineas){
-        conjunto_polilineas[i]=leer_polilinea(f);
-        i++;
-    }
-    figura->polilineas=conjunto_polilineas;
-
-    return figura;
-}
-
-void figura_destruir(figura_t *figura){
-    size_t j=0;
-    size_t i=figura->cantidad_polilineas;
-    printf("destruyendo polilineas");
-    while(j<i){
-        polilinea_destruir(figura->polilineas[i]);
-        printf("destruidas %zd figuras\n",j);
-        j++;
-    }
-    
-    free(figura);
-}
 
 void figura_trasladar(figura_t *figura, double traslacion[2]){
     for(size_t i=0; i<(figura->cantidad_polilineas);i++){
@@ -119,6 +80,52 @@ void figura_rotar(figura_t *figura, double angulo){
     }
 }
 
+//creacion 
+figura_t *figura_crear(FILE *f){
+    figura_t *figura=malloc(sizeof(figura_t));
+    if(figura==NULL)return NULL;
+    
+    if(leer_encabezado_figura(f,figura->nombre,&figura->tipo_figura,&figura->infinito,&figura->cantidad_polilineas)==0) return NULL;
+
+    size_t cantidad_polilineas=figura->cantidad_polilineas;
+
+    figura->polilineas=malloc(sizeof(polilinea_t*)*cantidad_polilineas);
+    
+    if(figura->polilineas==NULL){
+        figura_destruir(figura);
+        return NULL;
+    }
+    
+    size_t i=0;
+
+    while(i<cantidad_polilineas){
+        if((figura->polilineas[i]=leer_polilinea(f))==NULL)return NULL;
+        i++;
+    }
+
+    return figura;
+}
+figura_t **crear_figuras(FILE *f,size_t *i){
+    figura_t **bloque_figuras=malloc(sizeof(figura_t*));
+    if(!bloque_figuras) return NULL;
+
+    while((bloque_figuras[*i]=figura_crear(f))!=NULL){
+        /*if((bloque_figuras[*i]=figura_crear(f))==NULL){
+            destruir_bloque(bloque_figuras,(*i));
+            return NULL;
+        }*/
+        (*i)++;
+        figura_t **aux=realloc(bloque_figuras,sizeof(figura_t*)*((*i)+1));
+        if(!aux){
+            destruir_bloque(bloque_figuras,(*i));
+            return NULL;
+        }
+        bloque_figuras=aux;
+    }
+    return bloque_figuras;
+}
+
+
 //getters
 char *nombre_fig(const figura_t *figura){
     return (char *)figura->nombre;
@@ -126,8 +133,8 @@ char *nombre_fig(const figura_t *figura){
 bool infinito_fig(const figura_t *figura){
     return figura->infinito;
 }
-char *tipo_fig(const figura_t *figura){
-    return (char *)figura_tipo_a_cadena(figura->figura);
+figura_tipo_t tipo_fig(const figura_t *figura){
+    return (figura_tipo_t)figura->tipo_figura;
 }
 size_t cantidad_poli_fig(const figura_t *figura){
     return figura->cantidad_polilineas;
@@ -135,45 +142,76 @@ size_t cantidad_poli_fig(const figura_t *figura){
 polilinea_t **polilinea_fig(const figura_t *figura){
     return figura->polilineas;
 }
-figura_t *cargar(const figura_t **bloque,char *nombre_figura,size_t cantidad){
+
+//gestor de memoria
+figura_t *cargar_nombre(figura_t **bloque,char *nombre_figura){
     size_t i=0;
-    figura_t *figura=(figura_t *)bloque[i];
-    while(i<cantidad){       
+    figura_t *figura=NULL;
+    while(bloque[i]!=NULL){       
+        figura=(figura_t *)bloque[i];
         if(strcmp(figura->nombre,nombre_figura)==0)
             return figura;
         i++;
-        figura=(figura_t *)bloque[i];
     }
     return NULL;
 }
-
-
-//gestion de bloques
-figura_t **crear_figuras(FILE *f,size_t *i){
-    figura_t **bloque_figuras=malloc(sizeof(figura_t*));
-    if(!bloque_figuras) return NULL;
-    figura_t *figura0=figura_crear(f);
-
-    bloque_figuras[0]=figura0;
-    while((*i)<5){
-        (*i)++;
-        figura_t **aux=realloc(bloque_figuras,sizeof(figura_t)*((*i)+1));
-        if(!aux){
-            free(aux);
-            return NULL;
+figura_t **cargar_tipo(figura_t **bloque,char *tipo){
+    size_t i=0;
+    size_t j=0;
+    figura_t **figura=NULL;
+    while(bloque[i]!=NULL){
+        char *tipo=tipo_fig(bloque[i]);
+        if(strcmp(tipo,tipo)==0){
+            figura[j]=(figura_t *)bloque[i];
+            j++;
         }
-        bloque_figuras=aux;
-        bloque_figuras[*i]=figura_crear(f);
+        i++;
     }
-    return bloque_figuras;
+    if(j==0) return NULL;
+    return figura;
 }
+/*bool dibujar_poli(figura_t *figura, size_t cantidad,float escalado,float posicion[2]){
+    figura_t *dibujo=cargar(figura,nombre,cantidad);
+    if(!dibujo) return false;
+    
+    size_t cant_poli=figura->cantidad_polilineas;
+    
+    for(size_t i=0,i<cant_poli,i++){
+        polilinea_t *poli_dibu=figura->polilineas[i];
+        size_t cant_punt=polilinea_cantidad_puntos(poli_dibu);
+        float matriz[2][cant_punt];//revisar que esto sea legal
+        for(size_t j=0,j<cant_punt,j++){
+            //if(lodeabajo==0) return NULL;
+            polilinea_obtener_punto(poli_dibu,j,matriz[0][i],matriz[1][i]);// asi es la firma -> float *x, float *y
+        }
+        //existe polilinea_setear_color(polilinea_t *polilinea, color_t color)
+        //pero no le encuentro mucho sentido...
+        uint8_t r, g, b;
+        color_a_rgb(poli_dibu->color, &r, &g, &b); //en funciones.h habría que hacer #include "color.h"
+        SDL_SetRenderDrawColor(renderer, 0xFF, r, g, b);
+        //arreglar matriz[][] de las cosas de abajo
+        SDL_RenderDrawLine(render,(matriz[i][0]+posicion[0])*escalado,-(matriz[i][1]+posicion[1])*escalado,(matriz[i+1][0]+posicion[0])*escalado,-(matriz[i+1][1]+posicion[1])*escalado);
+    }
+    return true;
+}
+*/
 
+//destruir
+void figura_destruir(figura_t *figura){
+    size_t j=0;
+    size_t i=figura->cantidad_polilineas;
+    
+    while(j<i){
+        polilinea_destruir(figura->polilineas[j]);
+        j++;
+    }
+    free(figura->polilineas);
+    free(figura);
+}
 void destruir_bloque(figura_t **bloque,size_t i){
     size_t j=0;
-    printf("destruyendo figuras");
     while(j<i){
         figura_destruir(bloque[j]);
-        printf("destruidas %zd figuras\n",j);
         j++;
     }
     free(bloque);
--- game.c
+++ game.c
@@ -16,6 +16,48 @@ typedef enum{
     NIVEL1SW,
 }niveles_t;
 
+figura_t ***inicio(){
+
+    FILE *f = fopen("/figuras.bin", "rb");
+    if(f == NULL) {
+        fprintf(stderr, "No pudo abrirse el archivo\n");
+        return 1;
+    }
+    figura_t *auxiliar=NULL;
+    figura_t ***figuras=malloc(8*sizeof(figura_t ***));
+    if(figuras==NULL){
+        fclose(f);
+        return NULL;
+    }
+    figura_t **figuras_tipo=malloc(sizeof(figura_t **));
+    while((auxiliar=figura_crear(f))!=NULL){
+        tipo_fig(auxiliar);
+    }
+    size_t cantidad_figuras=0;
+    /*
+    figura_t **memoria=crear_figuras(f,&cantidad_figuras);
+
+    if(memoria==NULL){
+        printf("problema de memoria (NULL)");
+        fclose(f);
+        //destruir_bloque(memoria,cantidad_figuras);
+        return 1;
+    }
+    printf("\nson %zd figuras\n",cantidad_figuras);
+
+    figura_t *fig_generica=cargar_nombre(memoria,"NAVE");
+    printf("%s\n",nombre_fig(fig_generica));
+
+/*  figura_t **planetas=cargar_tipo(memoria,"Planeta");
+    figura_t *planeta_generico=cargar_nombre(planetas,"PLANETA4");
+    figura_t *planeta1=planetas[1];
+    printf("%s",nombre_fig(planeta_generico));
+*/
+//    destruir_bloque(memoria,cantidad_figuras);
+
+    fclose(f);
+}
+
 void render_figura(float pos[2]){
     
     

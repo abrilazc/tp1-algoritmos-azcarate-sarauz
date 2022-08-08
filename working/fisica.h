a9054fb0238ed7ecc25eebf76f729641374d8ca1
abril <abrilazcarate0@gmail.com>
2022-08-08 08:44:23 -0300

    1.6.0.1

--- working/fisica.h
+++ working/fisica.h
@@ -19,6 +19,8 @@ float dist_puntos(float a[2], float b[2]);
 
 void colision_rebote(nave_t *nave); //llamada en caso de colisiones que causan un rebote
 
+void colision_rebote_ni(nave_t *nave, bool *inicio); //llamada en caso de colisiones que causan un rebote
+
 void computar_posicion(nave_t *nave, float pos_g[2]);
 
 void trasladar(float pos[2],float v[2]); //llamada para mover un objeto

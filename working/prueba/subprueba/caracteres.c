7c2841e241c826f0d80348c448139a40460c6b0f
nachozr <109630879+nachozr@users.noreply.github.com>
2022-08-08 08:43:13 -0300

    kllñkñl

--- working/prueba/subprueba/caracteres.c
+++ working/prueba/subprueba/caracteres.c
@@ -1,4 +1,10 @@
 #include "caracteres.h"
+
+
+float **puntero=polilinea_caracter('a');
+
+polilinea_t *polilinea=polilinea_crear();
+
 float *caracteres[][2]={
 	['a']=caracter_a,
 	['b']=caracter_b,
@@ -45,64 +51,6 @@ float *caracteres[][2]={
 float **polilinea_caracter(char caracter){
 	return caracteres[caracter];
 }
-
-const size_t cantidad_filas(char caracter){
-    static const size_t tam_caracter[]={
-	['a']=sizeof(caracter_a)/(2*sizeof(float)),
-	['b']=sizeof(caracter_b)/(2*sizeof(float)),
-	['c']=sizeof(caracter_c)/(2*sizeof(float)),
-	['d']=sizeof(caracter_d)/(2*sizeof(float)),
-	['e']=sizeof(caracter_e)/(2*sizeof(float)),
-	['f']=sizeof(caracter_f)/(2*sizeof(float)),
-	['g']=sizeof(caracter_g)/(2*sizeof(float)),
-	['h']=sizeof(caracter_h)/(2*sizeof(float)),
-	['i']=sizeof(caracter_i)/(2*sizeof(float)),
-	['j']=sizeof(caracter_j)/(2*sizeof(float)),
-	['k']=sizeof(caracter_k)/(2*sizeof(float)),
-	['l']=sizeof(caracter_l)/(2*sizeof(float)),
-	['m']=sizeof(caracter_m)/(2*sizeof(float)),
-	['n']=sizeof(caracter_n)/(2*sizeof(float)),
-	['o']=sizeof(caracter_o)/(2*sizeof(float)),
-	['p']=sizeof(caracter_p)/(2*sizeof(float)),
-	['q']=sizeof(caracter_q)/(2*sizeof(float)),
-	['r']=sizeof(caracter_r)/(2*sizeof(float)),
-	['s']=sizeof(caracter_s)/(2*sizeof(float)),
-	['t']=sizeof(caracter_t)/(2*sizeof(float)),
-	['u']=sizeof(caracter_u)/(2*sizeof(float)),
-	['v']=sizeof(caracter_v)/(2*sizeof(float)),
-	['w']=sizeof(caracter_w)/(2*sizeof(float)),
-	['x']=sizeof(caracter_x)/(2*sizeof(float)),
-	['y']=sizeof(caracter_y)/(2*sizeof(float)),
-	['z']=sizeof(caracter_z)/(2*sizeof(float)),
-	['1']=sizeof(caracter_1)/(2*sizeof(float)),
-	['2']=sizeof(caracter_2)/(2*sizeof(float)),
-	['3']=sizeof(caracter_3)/(2*sizeof(float)),
-	['4']=sizeof(caracter_4)/(2*sizeof(float)),
-	['5']=sizeof(caracter_5)/(2*sizeof(float)),
-	['6']=sizeof(caracter_6)/(2*sizeof(float)),
-	['7']=sizeof(caracter_7)/(2*sizeof(float)),
-	['8']=sizeof(caracter_8)/(2*sizeof(float)),
-	['9']=sizeof(caracter_9)/(2*sizeof(float)),
-	['0']=sizeof(caracter_0)/(2*sizeof(float)),
-	['x']=sizeof(caracter_x)/(2*sizeof(float)),
-	['y']=sizeof(caracter_y)/(2*sizeof(float)),
-	['z']=sizeof(caracter_z)/(2*sizeof(float)),
-	[' ']=sizeof(caracter_espacio)/(2*sizeof(float))
-    };
-    return tam_caracter[caracter];
-}
-polilinea_t **palabra_a_polilinea(char *palabra){
-	
-}
-
-polilinea_t *letra_a_polilinea(char caracter){
-	size_t cantidad=cantidad_filas(caracter);
-	float **matriz=polilinea_caracter(caracter);
-	polilinea_t *polilinea=polilinea_crear(matriz,cantidad_filas,0);
-	return polilinea;
-}
-	
-
 
 
 const float caracter_a[7][2] = {

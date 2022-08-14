4d92151f9e1bf18d2e28b70396e4d3da962aa97d
nachozr <109630879+nachozr@users.noreply.github.com>
2022-08-14 16:27:54 -0300

    sdfasd

---
+++ working/main.c
@@ -0,0 +1,127 @@
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+#include "config.h"
+#include "figura.h"
+#include "game.h"
+#include "fisica.h"
+#include "nave.h"
+#include "pantalla.h"
+#include "nivel.h"
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+
+    SDL_CreateWindowAndRenderer(VENTANA_ANCHO, VENTANA_ALTO, 0, &window, &renderer);
+    SDL_SetWindowTitle(window, "Gravitar");
+
+    int dormir = 0;
+
+    // BEGIN código del alumno
+    // Mi nave:
+    size_t cantidad_figuras[8]={0,0,0,0,0,0,0,0};
+    figura_t ***figuras=inicio(cantidad_figuras);
+    if(figuras==NULL)
+        return 1;
+    bool pantalla_inicio_spawn=true;
+    bool nivel_inicio=true;
+    size_t cantidad_niveles=cantidad_figuras[1];
+    nivel_t **niveles=crear_niveles(figuras[1], cantidad_niveles);
+    nave_t *nave=nave_crear(figuras[2]);
+    planeta_nombre planeta_actual=NIVEL1NE;
+    float velocidad[2]={0,0};
+    float posicion[2] ={VENTANA_ANCHO/2,VENTANA_ALTO/2};
+    float centro=VENTANA_ANCHO/2;
+    nave_posicion_set(nave, posicion);
+    bool held_down=false;
+    size_t frame=0;
+    char puntaje[8]="PUNTAJE";
+    char combustible[12]="COMBUSTIBLE";
+    int tam_let=strlen(combustible);
+    float escala_letra=3;
+    float pos_puntaje[]={VENTANA_ANCHO*0.4,VENTANA_ALTO*0.9};
+    float pos_p[]={VENTANA_ANCHO*0.4+tam_let*ESP_LET*escala_letra,VENTANA_ALTO*0.9};
+    float pos_comb[]={VENTANA_ANCHO*0.4,VENTANA_ALTO*0.95};
+    float pos_c[]={VENTANA_ANCHO*0.4+tam_let*ESP_LET*escala_letra,VENTANA_ALTO*0.95};
+    //float escala=1;
+    
+    
+    // Queremos que todo se dibuje escalado por f:
+    float f = 1;
+    //listas(nave,niveles,figuras,renderer,planeta_actual,f);
+    // END código del alumno
+    
+    unsigned int ticks = SDL_GetTicks();
+    
+    while(1) {
+        frame++;
+        
+        //printf("\nframe: %zd posicion: (%.2f,%.2f) velocidad: (%.2f,%.2f)  ",frame,posicion[0],posicion[1],velocidad[0],velocidad[1]);
+        if(SDL_PollEvent(&event)){
+            if (event.type == SDL_QUIT)
+                break;
+            // BEGIN código del alumno
+            registrar_teclas(nave, event, &held_down);
+            // END código del alumno
+            continue;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);  
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
+        
+        // BEGIN código del alumno
+        // Dibujamos la nave escalada por f en el centro de la pantalla:
+        palabra_a_polilinea(renderer,puntaje,pos_puntaje,false,true,true,escala_letra);
+        numero_a_polilinea(10500,renderer,pos_p,false,true,false,escala_letra);
+        palabra_a_polilinea(renderer,combustible,pos_comb,false,true,true,escala_letra);
+        numero_a_polilinea(combustible_get(nave),renderer,pos_c,false,true,false,escala_letra);
+        if(nivel_inicio){
+            
+            if(pantalla_inicio_spawn){
+                cargar_pantalla_inicio(nave,niveles,figuras,planeta_actual,pantalla_inicio_spawn);
+                pantalla_inicio_spawn=false;
+                //palabra_a_polilinea(renderer,"PUNTAJE",pos_puntaje,false,true,false);
+            }
+            nivel_inicio=pantalla_inicio_mostrar(nave, figuras, niveles, renderer, &f, &planeta_actual);
+            }
+        else{
+            pantalla_nivel(nave,figuras,niveles,renderer,&nivel_inicio,&planeta_actual,&f, &centro);
+            
+            //listas(nave,niveles,figuras,renderer,planeta_actual,f);
+
+        }
+        
+        nave_velocidad_get(nave, velocidad);
+        nave_posicion_get(nave, posicion);
+        printf("frame:%ld posicion:(%.2f,%.2f\n)",frame,posicion[0],posicion[1]);
+        // END código del alumno
+
+        SDL_RenderPresent(renderer);
+        ticks = SDL_GetTicks() - ticks;
+        if(dormir) {
+            SDL_Delay(dormir);
+            dormir = 0;
+        }
+        else if(ticks < 1000 / JUEGO_FPS)
+            SDL_Delay(1000 / JUEGO_FPS - ticks);
+        ticks = SDL_GetTicks();
+    }
+    
+    // BEGIN código del alumno
+    nave_destruir(nave);
+    destruir_niveles(niveles, cantidad_niveles);
+    destruir_figuras(figuras, cantidad_figuras);
+    // END código del alumno
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
+
---
+++ working/prueba/prueba/nave.c
@@ -0,0 +1,167 @@
+#include "nave.h"
+struct nave{ 
+    figura_t **figura;
+    float posicion[2];
+    float velocidad[2];
+    float direccion;
+    float combustible;
+    bool escudo;
+    bool chorro;
+    uint8_t vidas;
+    size_t puntos_relativos;
+    size_t puntos;
+};
+
+static void nave_rotar(nave_t *nave, float angulo){
+    for(size_t i=0;i<4;i++){
+        figura_rotar(*(nave->figura+i),angulo);
+    }
+}
+
+nave_t *nave_crear(figura_t **figura){
+    nave_t *nave=malloc(sizeof(nave_t));
+    if(nave==NULL)
+        return NULL;
+    nave->figura=figura;
+    nave->escudo=false;
+    nave->chorro=false;
+    nave->direccion=NAVE_ANGULO_INICIAL;
+    nave_rotar(nave, nave->direccion);
+    *(nave->posicion)=0;
+    *(nave->posicion+1)=0;
+    *(nave->velocidad)=0;
+    *(nave->velocidad+1)=0;
+    nave->combustible=JUEGO_COMBUSTIBLE_INICIAL;
+    nave->vidas=3;
+    nave->puntos=0;
+    nave->puntos_relativos=0;
+    
+
+    return nave;
+}
+
+//setters
+void nave_posicion_set(nave_t *nave, float posicion[2]){
+    nave->posicion[0]=posicion[0];
+    nave->posicion[1]=posicion[1];
+}
+
+void nave_velocidad_set(nave_t *nave, float velocidad[2]){
+    *(nave->velocidad)=velocidad[0];
+    *(nave->velocidad+1)=velocidad[1];
+}
+
+void nave_direccion_set(nave_t *nave, float direccion){
+    (nave->direccion)=direccion;
+}
+
+void chorro_set(nave_t *nave){
+    nave->chorro=true;
+}
+void chorro_clear(nave_t *nave){
+    nave->chorro=false;
+}
+void escudo_set(nave_t *nave){
+    nave->escudo=true;
+}
+void escudo_clear(nave_t *nave){
+    nave->escudo=false;
+}
+
+void nave_respawnear(nave_t *nave, float posicion[2]){
+    nave_rotar(nave,(PI/2.0)-(nave->direccion));
+    nave_posicion_set(nave, posicion);
+    float velocidad[2]={0,0};
+    nave_velocidad_set(nave,velocidad);
+    nave->combustible=JUEGO_COMBUSTIBLE_INICIAL;
+}
+
+void vidas_incrementar(nave_t *nave){
+    nave->vidas++;
+}
+bool vidas_decrementar(nave_t *nave){
+    if(nave->vidas==0)
+        return false;
+    nave->vidas--;
+    return true;
+}
+
+void combustible_cargar(nave_t *nave){
+    nave->combustible+=3000;
+}
+bool combustible_usar(nave_t *nave){
+    if(nave->chorro)
+        nave->combustible-=(JUEGO_COMBUSTIBLE_POT_X_SEG/JUEGO_FPS);    
+    if(nave->escudo)
+        nave->combustible-=(JUEGO_COMBUSTIBLE_ESC_X_SEG/JUEGO_FPS);
+    if(nave->combustible<=0)
+        return false;
+    else
+        return true;
+}
+void nave_rotar_horario(nave_t *nave){
+    (nave->direccion)-=NAVE_ROTACION_PASO;
+    printf("\nhorario direccion: %.2f",nave->direccion);
+    nave_rotar(nave,-(NAVE_ROTACION_PASO));
+}
+void nave_rotar_antihorario(nave_t *nave){
+    (nave->direccion)+=NAVE_ROTACION_PASO;
+    printf("\nantihorario direccion: %.2f",nave->direccion);
+    nave_rotar(nave,(NAVE_ROTACION_PASO));
+    if(nave->direccion>=2*PI)
+        nave->direccion-=2*PI;
+}
+void puntos_torreta(nave_t *nave){
+    nave->puntos+=250;
+    nave->puntos_relativos+=250;
+    if(nave->puntos_relativos>=10000){
+        vidas_incrementar(nave);
+        nave->puntos_relativos-=10000;
+    }
+}
+void puntos_nivel(nave_t *nave, size_t puntos){
+    nave->puntos+=puntos;
+    nave->puntos_relativos+=puntos;
+    if(nave->puntos_relativos>=10000){
+        vidas_incrementar(nave);
+        nave->puntos_relativos-=10000;
+    }
+}
+
+//getters
+bool chorro_get(nave_t *nave){
+    return nave->chorro;
+}
+bool escudo_get(nave_t *nave){
+    return nave->escudo;
+}
+void nave_posicion_get(nave_t *nave, float posicion[2]){
+    posicion[0]=*(nave->posicion);
+    posicion[1]=*(nave->posicion+1);
+}
+
+void nave_velocidad_get(nave_t *nave, float velocidad[2]){
+    velocidad[0]=*(nave->velocidad);
+    velocidad[1]=*(nave->velocidad+1);
+}
+
+double direccion_get(nave_t *nave){
+    return nave->direccion;
+}
+void velocidad_get(nave_t *nave, float velocidad[2]){
+    *(nave->velocidad)=velocidad[0];
+    *(nave->velocidad+1)=velocidad[1];
+}
+uint8_t vidas_get(nave_t *nave){
+    return nave->vidas;
+    
+}
+float combustible_get(nave_t *nave){
+    return  nave->combustible;
+}
+
+//destruccion
+
+void nave_destruir(nave_t *nave){    
+    free(nave);
+}
